701cf7335e4c64dc38edbcb56559187c685aee02
Diego Yanez <diegosyanez92@gmail.com>
2018-04-16 20:02:59 +0000

    automatically backed up by learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -17,7 +17,8 @@ class MP3Importer
   end
 
   def import
-    @files.each{|file_name| Song.new_by_filename(file_name)}
+    files
+    @files.each {|file_name| Song.new_by_filename(file_name)}
   end
 
 
