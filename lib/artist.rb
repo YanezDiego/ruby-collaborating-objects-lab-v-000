348baad987e4ea46173d36b1092c1d19075a1644
Diego Yanez <diegosyanez92@gmail.com>
2018-04-16 19:29:40 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -2,20 +2,24 @@ require 'pry'
 class Artist
   attr_accessor :artist, :name, :songs
 
-  def initialize(name)
-    @name = name
-    @songs = []
-  end
+  @@all = []
 
-  def add_song(song)
-    @songs << song
-    song.artist = self
-    @song
-  end
+    def initialize(name)
+      @name = name
+      @songs = []
+    end
 
-  def songs
-    @songs
-  end
+    def add_song(song)
+      @songs << song
+      song.artist = self
+      @song
+    end
+
+    def songs
+      @songs
+    end
+
+    def save
 
 
 
